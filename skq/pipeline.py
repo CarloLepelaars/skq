8aa13a1355efa3094c990b1134bc04feadbb7d98
Carlo Lepelaars <info@carlolepelaars.nl>
2024-07-19 13:23:09 +0200

    Get QuantumFeatureUnion working on combinations of multi qubit and single qubit states. QuantumFeatureUnion checks and tests

--- skq/pipeline.py
+++ skq/pipeline.py
@@ -1,32 +1,73 @@
 import numpy as np
-from skq.transformers import ITransformer
-from skq.gates import IGate
+from skq.transformers import SingleQubitTransformer, MultiQubitTransformer
+from skq.gates import Gate, IGate
 from sklearn.pipeline import _name_estimators, Pipeline, FeatureUnion
+
 
 class QuantumFeatureUnion(FeatureUnion):
     def __init__(self, transformer_list, n_qubits, **kwargs):
         self.n_qubits = n_qubits
         super().__init__(transformer_list, **kwargs)
 
-    def transform(self, X):
+    def transform(self, X) -> np.array:
         combined_gate = np.eye(2**self.n_qubits, dtype=complex)
-        
+        used_qubits = set()
         for _, transformer in self.transformer_list:
-            # Construct the full gate for this transformer
-            full_gate = np.eye(1, dtype=complex)
-            for i in range(self.n_qubits):
-                if i in transformer.qubits:
-                    full_gate = np.kron(full_gate, transformer.gate)
-                else:
-                    full_gate = np.kron(full_gate, IGate())
-            
-            # Combine with the overall gate
-            combined_gate = np.dot(combined_gate, full_gate)
-        
-        # Apply the combined gate to the input state vectors
-        transformed_X = np.array([combined_gate @ x for x in X])
-        return transformed_X
+            if not hasattr(transformer, 'qubits'):
+                raise ValueError(f"Transformers in QuantumFeatureUnion must be either type `SingleQubitTransformer` or `MultiQubitTransformer`. Got '{type(transformer)}'")
+            # Check for transformers trying to use the same qubit
+            for qubit in transformer.qubits:
+                if qubit in used_qubits:
+                    raise ValueError(f"Qubit {qubit} is used by multiple transformers.")
+                used_qubits.add(qubit)
 
+            full_gate = self._construct_full_gate(transformer)
+            combined_gate = np.dot(combined_gate, full_gate)
+        return np.array([combined_gate @ x for x in X])
+
+    def _construct_full_gate(self, transformer):
+        if isinstance(transformer, SingleQubitTransformer):
+            return self._expand_single_qubit_gate(transformer.gate, transformer.qubits[0])
+        elif isinstance(transformer, MultiQubitTransformer):
+            return self._expand_multi_qubit_gate(transformer.gate, transformer.qubits)
+        else:
+            raise ValueError(f"Transformers in QuantumFeatureUnion must be either type `SingleQubitTransformer` or `MultiQubitTransformer`. Got '{type(transformer)}'")
+
+    def _expand_single_qubit_gate(self, gate: Gate, qubit: int) -> np.array:
+        """
+        Expands a single qubit gate to a full gate.
+        :param gate: 2x2 numpy array representing the gate.
+        :param qubit: Index of the qubit the gate is applied to.
+        :return: 2**n_qubits x 2**n_qubits numpy array representing the full gate.
+        """
+        full_gate = np.eye(1, dtype=complex)
+        for i in range(self.n_qubits):
+            if i == qubit:
+                full_gate = np.kron(full_gate, gate)
+            else:
+                full_gate = np.kron(full_gate, IGate())
+        return full_gate
+
+    def _expand_multi_qubit_gate(self, gate: Gate, qubits: list[int]) -> np.array:
+        """
+        Expands a multi qubit gate to a full gate.
+        :param gate: numpy array representing the gate.
+        :param qubits: List of qubits the gate is applied to.
+        :return: 2**n_qubits x 2**n_qubits numpy array representing the full gate.
+        """
+        full_gate = np.eye(2**self.n_qubits, dtype=complex)
+        num_controls = len(qubits) - 1
+        for i in range(2**self.n_qubits):
+            binary = f"{i:0{self.n_qubits}b}"
+            if all(binary[q] == '1' for q in qubits[:-1]):
+                target = qubits[-1]
+                for k in range(2**num_controls):
+                    control_state = f"{k:0{num_controls}b}"
+                    control_index = int(''.join(control_state), 2)
+                    full_gate[i ^ (control_index << target), i ^ (control_index << target)] = gate[control_index, control_index]
+                    full_gate[i ^ (control_index << target), i] = gate[control_index, 0]
+                    full_gate[i, i ^ (control_index << target)] = gate[0, control_index]
+        return full_gate
 
 def make_quantum_union(*transformers, n_qubits: int , n_jobs=None, verbose=False) -> QuantumFeatureUnion:
     """
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,9 +1,10 @@
+import pytest
 import numpy as np
 
 from skq.pipeline import QuantumFeatureUnion
-from skq.transformers import HTransformer, ZTransformer
+from skq.transformers import HTransformer, ZTransformer, CXTransformer, TTransformer, U3Transformer, SWAPTransformer
 
-def test_quantum_feature_union():
+def test_quantum_feature_union_single_qubits():
     H_transformer = HTransformer(qubits=[0])
     Z_transformer = ZTransformer(qubits=[1])
     
@@ -13,3 +14,71 @@ def test_quantum_feature_union():
     
     expected_combined_gate = np.kron(H_transformer.gate, Z_transformer.gate)
     assert np.allclose(transformed_X, [expected_combined_gate @ x for x in X])
+
+def test_quantum_feature_union_single_multi_qubit():
+    CX_transformer = CXTransformer(qubits=[0, 1])
+    T_transformer = TTransformer(qubits=[2])
+    
+    qfu = QuantumFeatureUnion([('cnot', CX_transformer), ('t-gate', T_transformer)], n_qubits=3)
+    # Initial state |110>
+    X = np.array([[0, 0, 0, 0, 0, 1, 0, 0]], dtype=complex)
+    transformed_X = qfu.transform(X)
+
+    # Expected final state |101> with phase e^(i*pi/4) -> [0, 0, 0, 0, 0.70710678 + 0.70710678j, 0, 0, 0]
+    expected_output = np.array([[0, 0, 0, 0, 0, 0.70710678 + 0.70710678j, 0, 0]], dtype=complex)
+    assert np.allclose(transformed_X, expected_output)
+
+def test_quantum_feature_union_single_multi_qubit_swap():
+    SWAP_transformer = SWAPTransformer(qubits=[0, 1])
+    T_transformer = TTransformer(qubits=[2])
+
+    qfu = QuantumFeatureUnion([('swap', SWAP_transformer), ('t-gate', T_transformer)], n_qubits=3)
+    
+    # Initial state |100> -> [0, 0, 0, 0, 0, 0, 0, 1]
+    X = np.array([[0, 0, 0, 0, 0, 0, 0, 1]], dtype=complex)
+    transformed_X = qfu.transform(X)
+    
+    # Expected final state |011> with phase e^(i*pi/4) -> [0, 0, 0, 0, 0, 0, 0, 0.70710678+0.70710678j]
+    expected_output = np.array([[0, 0, 0, 0, 0, 0, 0, 0.70710678 + 0.70710678j]], dtype=complex)
+    assert np.allclose(transformed_X, expected_output)
+
+def test_quantum_feature_union_multiple_single_qubits():
+    H_transformer = HTransformer(qubits=[0])
+    T_transformer = TTransformer(qubits=[1])
+    U3_transformer = U3Transformer(theta=np.pi, phi=np.pi, lam=np.pi, qubits=[2])
+    
+    qfu = QuantumFeatureUnion([
+        ('hadamard', H_transformer),
+        ('t-gate', T_transformer),
+        ('u3', U3_transformer)
+    ], n_qubits=3)
+    X = np.array([[1, 0, 0, 0, 0, 0, 0, 0]], dtype=complex)
+    transformed_X = qfu.transform(X)
+    
+    expected_combined_gate = np.kron(H_transformer.gate, np.kron(T_transformer.gate, U3_transformer.gate))
+    assert np.allclose(transformed_X, [expected_combined_gate @ x for x in X])
+
+def test_clashing_qubits():
+    CX_transformer = CXTransformer(qubits=[0, 1])
+    SWAP_transformer = SWAPTransformer(qubits=[1, 2])
+
+    qfu = QuantumFeatureUnion([('cnot', CX_transformer), ('swap', SWAP_transformer)], n_qubits=3)
+    
+    initial_state = np.array([[1, 0, 0, 0, 0, 0, 0, 0]], dtype=complex)
+    
+    # Expect ValueError due to clashing qubits
+    with pytest.raises(ValueError):
+        qfu.transform(initial_state)
+
+def test_invalid_transformer():
+    class InvalidTransformer:
+        pass
+
+    invalid_transformer = InvalidTransformer()
+    qfu = QuantumFeatureUnion([('invalid', invalid_transformer)], n_qubits=1)
+    
+    initial_state = np.array([[1, 0]], dtype=complex)
+    
+    # Expect ValueError due to invalid transformer
+    with pytest.raises(ValueError):
+        qfu.transform(initial_state)
