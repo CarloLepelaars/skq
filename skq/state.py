a6a87f0fa19d4abf4b9e2e4d0db89b847468d1b3
Carlo Lepelaars <info@carlolepelaars.nl>
2024-07-21 20:04:11 +0200

    Statevector checks. measure bits

--- skq/state.py
+++ skq/state.py
@@ -6,7 +6,7 @@ from skq.density import DensityMatrix
 
 class Statevector(np.ndarray):
     """
-    State vector representation for a quantum state.
+    Statevector representation for a quantum (qubit) state.
     NOTE: Statevector is assumed to be constructed in big-endian format.
     Little-endian -> Least significant qubit (LSB) is on the right. Like |q1 q0> where q0 is the LSB.
     Big-endian -> Least significant qubit (LSB) is on the left. Like |q0 q1> where q0 is the LSB.
@@ -16,6 +16,7 @@ class Statevector(np.ndarray):
         obj = arr.view(cls)
         assert obj.is_1d(), "State vector must be 1D."
         assert obj.is_normalized(), "State vector must be normalized."
+        assert obj.is_power_of_two_len(), "State vector length must be a power of 2."
         return obj
     
     def is_1d(self) -> bool:
@@ -26,6 +27,19 @@ class Statevector(np.ndarray):
         """ Check if the state vector is normalized: ||ψ|| = 1. """
         return np.isclose(np.linalg.norm(self), 1)
     
+    def is_power_of_two_len(self) -> bool:
+        """ Check if a number is a power of two """
+        n = len(self)
+        return (n > 0) and (n & (n - 1)) == 0
+    
+    def num_qubits(self) -> int:
+        """ Return the number of qubits in the state vector. """
+        return int(np.log2(len(self)))
+    
+    def is_multi_qubit(self) -> bool:
+        """ Check if the state vector represents a multi-qubit state. """
+        return self.num_qubits() > 1
+    
     def density_matrix(self) -> DensityMatrix:
         """ Return the density matrix representation of the state vector. """
         return DensityMatrix(np.outer(self, self.conj()))
@@ -34,13 +48,20 @@ class Statevector(np.ndarray):
         """ Return the probabilities of all possible states. """
         return np.abs(self) ** 2
     
-    def measure(self, size: int = 1) -> int:
+    def measure_index(self) -> int:
         """ 
         Simulate a measurement of the state vector using the amplitudes. 
         :param size: Number of measurements to simulate
         :return: Index of the measured state
         """
-        return np.random.choice(len(self), p=self.probabilities(), size=size)
+        return np.random.choice(len(self), p=self.probabilities())
+    
+    def measure_bitstring(self) -> str:
+        """ 
+        Simulate a measurement of the state vector using the amplitudes. 
+        :return: Bitstring representation of the measured state
+        """
+        return bin(self.measure_index())[2:].zfill(self.num_qubits())
     
     def reverse(self) -> 'Statevector':
         """ Reverse the order of the state vector to account for endianness. 
--- tests/test_state.py
+++ tests/test_state.py
@@ -6,29 +6,61 @@ from skq.state import Statevector
 from skq.density import DensityMatrix
 
 def test_statevector_initialization():
-    state_vector = Statevector([1, 0, 0])
+    # |00>
+    state_vector = Statevector([1, 0, 0, 0])
     assert isinstance(state_vector, Statevector)
     assert state_vector.is_normalized()
 
+    # Bell state (|00> + |11>) / sqrt(2)
+    state_vector = Statevector([1, 0, 0, 1] / np.sqrt(2))
+    assert isinstance(state_vector, Statevector)
+    assert state_vector.is_normalized()
+    
     # Non-normalized state vector
     with pytest.raises(AssertionError):
-        Statevector([1, 1, 1])
+        Statevector([1, 1])
+
+    # Non-power of two state vector
+    with pytest.raises(AssertionError):
+        Statevector([1, 0, 0])
+
+    # State vector not 1D (like a matrix)
+    with pytest.raises(AssertionError):
+        Statevector([[1, 0], [0, 1]])
 
 def test_density_matrix():
-    state_vector = Statevector([1, 0, 0])
+    # |0><0|
+    state_vector = Statevector([1, 0])
     density_matrix = state_vector.density_matrix()
     assert isinstance(density_matrix, DensityMatrix)
-    expected_density_matrix = np.array([[1, 0, 0], [0, 0, 0], [0, 0, 0]])
+    expected_density_matrix = np.array([[1, 0], [0, 0]])
     np.testing.assert_array_almost_equal(density_matrix, expected_density_matrix)
 
+def test_measure():
+    # |00>
+    state_vector = Statevector([1, 0, 0, 0])
+    measured_state = state_vector.measure_index()
+    assert measured_state == 0
+    bitstring = state_vector.measure_bitstring()
+    assert bitstring == "00"
+
+    # Bell state (|00> + |11>) / sqrt(2)
+    state_vector = Statevector([1, 0, 0, 1] / np.sqrt(2))
+    measured_state = state_vector.measure_index()
+    assert measured_state in [0, 3]
+    bitstring = state_vector.measure_bitstring()
+    assert bitstring in ["00", "11"]
+
 def test_to_qiskit():
-    state_vector = Statevector([1, 0, 0])
+    # |00> in big-endian
+    state_vector = Statevector([1, 0, 0, 0])
     qiskit_sv = state_vector.to_qiskit()
     assert isinstance(qiskit_sv, qiskit.quantum_info.Statevector)
     np.testing.assert_array_almost_equal(qiskit_sv.data, state_vector[::-1])
 
 def test_from_qiskit():
-    qiskit_sv = qiskit.quantum_info.Statevector([0, 0, 1])
+    # |00> in little-endian
+    qiskit_sv = qiskit.quantum_info.Statevector([0, 0, 0, 1])
     state_vector = Statevector.from_qiskit(qiskit_sv)
     assert isinstance(state_vector, Statevector)
     np.testing.assert_array_almost_equal(state_vector, qiskit_sv.data[::-1])
