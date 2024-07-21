98604317090d652777fb3e9698d61ac6581ee5c4
Carlo Lepelaars <info@carlolepelaars.nl>
2024-07-21 12:53:13 +0200

    todo encoders

--- skq/encoders.py
+++ skq/encoders.py
@@ -9,9 +9,21 @@ class BaseEncoder(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
+class BasisEncoder(BaseEncoder):
+    ...
 
 class AmplitudeEncoder(BaseEncoder):
     def transform(self, X):
         return X / np.linalg.norm(X)
-
     
+class RepeatedAmplitudeEncoder(BaseEncoder):
+    ...
+
+class AngleEncoder(BaseEncoder):
+    ...
+
+class CoherentStateEncoder(BaseEncoder):
+    ...
+
+class TimeEvolutionEncoder(BaseEncoder):
+    ...
