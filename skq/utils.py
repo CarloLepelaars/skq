96af47db717622e6a1fb29d85f475a6291250ea6
Carlo Lepelaars <info@carlolepelaars.nl>
2024-08-17 12:38:45 +0200

    Bell state notebook. to_bitstring

--- skq/utils.py
+++ skq/utils.py
@@ -20,3 +20,16 @@ def _check_quantum_state_array(X):
     num_qubits = int(np.log2(n_states))
     assert n_states == 2**num_qubits, f"Expected state vector length {2**num_qubits}, got {n_states}."
     return True
+
+def to_bitstring(arr: np.array) -> list[str]:
+    """
+    Convert a 2D binary array to a list of bitstrings.
+    :param arr: A 2D binary array
+    For example:
+    [[0, 1],
+     [1, 0]]
+    :return: List of bitstrings
+    For example:
+    ['01', '10']
+    """
+    return [''.join(map(str, m)) for m in arr]
