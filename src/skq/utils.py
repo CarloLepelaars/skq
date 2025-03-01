bd0cbedff7bc1750f477039d288c3bd238eecf31
Carlo Lepelaars <info@carlolepelaars.nl>
2025-03-01 14:05:57 +0100

    Add utils

--- src/skq/utils.py
+++ src/skq/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from plotly import graph_objects as go
 
 
 def _check_quantum_state_array(X):
@@ -35,3 +36,74 @@ def to_bitstring(arr: np.array) -> list[str]:
     ['01', '10']
     """
     return ["".join(map(str, m)) for m in arr]
+
+
+def matrix_heatmap(matrix: np.array, title: str = "Matrix Heatmap") -> go.Figure:
+    """
+    Visualize a matrix as a heatmap.
+    :param matrix: A 2D array
+    :param title: Title of the plot
+    :return: A plotly figure
+    """
+    fig = go.Figure(data=go.Heatmap(z=np.real(matrix), colorscale="RdBu", zmin=-1, zmax=1, text=np.real(matrix).round(3), texttemplate="%{text}", textfont={"size": 10}, hoverongaps=False))
+    fig.update_layout(title=title, xaxis_title="Column Index", yaxis_title="Row Index")
+    return fig
+
+
+def bloch(state: np.array) -> go.Figure:
+    """
+    Plot single qubit state on a Bloch sphere (3D plot).
+    :param state: A statevector array for a single qubit
+    :return: A plotly figure
+    """
+
+    def calculate_coordinates(theta, phi):
+        x = np.sin(theta) * np.cos(phi)
+        y = np.sin(theta) * np.sin(phi)
+        z = np.cos(theta)
+        return x, y, z
+
+    fig = go.Figure()
+    alpha, beta = state[0], state[1]
+    theta = 2 * np.arccos(np.abs(alpha))
+    phi = np.angle(beta) - np.angle(alpha)
+    x, y, z = calculate_coordinates(theta, phi)
+    surface_phi, surface_theta = np.mgrid[0 : 2 * np.pi : 100j, 0 : np.pi : 50j]
+    xs, ys, zs = calculate_coordinates(surface_theta, surface_phi)
+
+    fig.add_trace(go.Surface(x=xs, y=ys, z=zs, opacity=0.5, colorscale="Blues", showscale=False))
+
+    fig.add_trace(go.Scatter3d(x=[0, x], y=[0, y], z=[0, z], mode="lines+markers+text", marker=dict(size=10, color="green"), line=dict(color="green", width=8), textposition="top center", showlegend=True, name=f"{alpha:.3f}|0⟩ + {beta:.3f}|1⟩"))
+
+    fig.add_trace(go.Scatter3d(x=[0, 0, 1, -1, 0, 0], y=[0, 0, 0, 0, 1, -1], z=[1, -1, 0, 0, 0, 0], mode="markers", marker=dict(size=5, color="black"), hovertext=["|0⟩", "|1⟩", "|+⟩", "|-⟩", "|i⟩", "|-i⟩"], showlegend=False, name="Basis states"))
+
+    boundary_phi = np.linspace(0, 2 * np.pi, 100)
+    coords = [(np.cos(boundary_phi), np.sin(boundary_phi), np.zeros_like(boundary_phi)), (np.zeros_like(boundary_phi), np.cos(boundary_phi), np.sin(boundary_phi)), (np.cos(boundary_phi), np.zeros_like(boundary_phi), np.sin(boundary_phi))]
+
+    for x, y, z in coords:
+        fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode="lines", line=dict(color="black", width=2), showlegend=False, name="Axes"))
+
+    fig.update_layout(
+        legend=dict(
+            font=dict(size=20),
+            x=0.05,
+            y=0.95,
+            xanchor="left",
+            yanchor="top",
+            bgcolor="rgba(0,0,0,0)",
+        ),
+        margin=dict(l=0, r=0, t=0, b=0),
+    )
+    return fig
+
+
+def measure_state(state: np.array) -> int:
+    """
+    Measure a quantum state and return a classical bit.
+    :param state: A statevector array
+    :return: A classical bit
+    """
+    # Absolute value squared gives us the probability distribution from the statevector
+    p = np.abs(state) ** 2
+    # Sample from probability distribution to get classical bits
+    return np.random.choice([0, 1], p=p)
